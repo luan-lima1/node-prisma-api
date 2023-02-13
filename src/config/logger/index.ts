81bb3eaebf10a2c43cdd5566045f1cd6dff069be
Luan-Andriel <luan@cfsoftware.dev>
2023-02-13 13:59:04 -0300

    Base Project

---
+++ src/config/logger/index.ts
@@ -0,0 +1,10 @@
+import pino from "pino";
+import createConfig from "../load";
+
+const config = createConfig();
+const { enabled, loggerInfo } = config.logger;
+
+export default pino({
+  enabled,
+  level: loggerInfo,
+});
---
+++ src/enums/statusCodes.ts
@@ -0,0 +1,10 @@
+enum StatusCodes {
+  OK = 200,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
+  BAD_GATEWAY = 502,
+}
+
+export {StatusCodes};
