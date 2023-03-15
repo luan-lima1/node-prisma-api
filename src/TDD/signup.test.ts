121516f2280074e7050997521939f5e592f9d2d5
Luan Andriel <luan.andriel.3.0@gmail.com>
2023-03-15 13:28:35 -0300

    Email Validator Were Moved

--- src/TDD/signup.test.ts
+++ src/TDD/signup.test.ts
@@ -10,14 +10,25 @@ interface SutTypes {
   sut: SignUpController;
   emailValidatorStub: EmailValidator;
 }
-
-const makeSut = (): SutTypes => {
+const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid(email: string): boolean {
       return true;
     }
   }
-  const emailValidatorStub = new EmailValidatorStub();
+  return new EmailValidatorStub();
+};
+
+const makeEmailValidatorWithError = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      throw new Error();
+    }
+  }
+  return new EmailValidatorStub();
+};
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator();
   const sut = new SignUpController(emailValidatorStub);
   return {
     sut,
@@ -96,12 +107,7 @@ test("should call EmailValidator with a correct email", () => {
 });
 
 test("should return 500 if EmailValidator throws", () => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid(email: string): boolean {
-      throw new Error();
-    }
-  }
-  const emailValidatorStub = new EmailValidatorStub();
+  const emailValidatorStub = makeEmailValidatorWithError();
   const sut = new SignUpController(emailValidatorStub);
   const httpRequest = {
     body: {
